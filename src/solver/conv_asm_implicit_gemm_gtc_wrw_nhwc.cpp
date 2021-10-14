9b8282b77363e398fad254e8bb740be1e354491a
shaojiewang <wsjmessi@163.com>
2021-10-14 14:26:33 +0800

    fix bug for wrw fp16 nhwc kernel selection

--- src/solver/conv_asm_implicit_gemm_gtc_wrw_nhwc.cpp
+++ src/solver/conv_asm_implicit_gemm_gtc_wrw_nhwc.cpp
@@ -470,6 +470,8 @@ void PerformanceConfigAsmImplicitGemmGTCWrwXdlopsNHWC::HeuristicInit(const Convo
             if(miopen::IsDisabled(MIOPEN_DEBUG_CONV_IMPLICIT_GEMM_ASM_PK_ATOMIC_ADD_FP16{}))
                 if(ctx.IsFp16() && tensor_b_thread_lengths[3] != 1)
                     vector_store = 1;
+            if(ctx.IsFp16() && tensor_b_thread_lengths[3] == 1)
+                vector_store = 1;
             gemm_k_global_split = occupancy;
         }
     }
@@ -503,6 +505,8 @@ void PerformanceConfigAsmImplicitGemmGTCWrwXdlopsNHWC::HeuristicInit(const Convo
                         if(miopen::IsDisabled(MIOPEN_DEBUG_CONV_IMPLICIT_GEMM_ASM_PK_ATOMIC_ADD_FP16{}))
                             if(ctx.IsFp16() && tensor_b_thread_lengths[3] != 1)
                                 vector_store = 1;
+                        if(ctx.IsFp16() && tensor_b_thread_lengths[3] == 1)
+                            vector_store = 1;
                         gemm_k_global_split = occupancy;
                     }
                     return;
